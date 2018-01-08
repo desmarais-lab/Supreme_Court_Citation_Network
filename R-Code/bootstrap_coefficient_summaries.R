dd27f557092b236bf53bd10a08f0a3dfd7beadf7
Christian <cxs5700@psu.edu>
2018-01-08 12:02:45 -0500

    updated bootstrap_coefficient_summaries.R
    added code for model without endogenous statistics

--- R-Code/bootstrap_coefficient_summaries.R
+++ R-Code/bootstrap_coefficient_summaries.R
@@ -168,3 +168,111 @@ polygon(c(sender_years,rev(sender_years)),c(numberjusticespro_coef_trend[,2],rev
 lines(sender_years,numberjusticespro_coef_trend[,1],lwd=2,col="grey50")
 abline(h=0,lty=2,lwd=2)
 dev.off()
+
+
+
+########################################################################
+#### same for the model without endogenous statistics
+########################################################################
+
+
+# code assumes that working directory is set
+# to top-level project
+bootstrapResults <- read.csv("bootresults_noEndo.csv",stringsAsFactors=F,row.names=1)
+
+# Christian, can you correct this? The sender time values should be set to map onto the range
+# of sender times in our data
+sender_times <- 1:65
+sender_years <- 1937:2001
+
+density_coefficients <- c("edges","nodeocov.sender.time")
+mq_coefficients <- c("edgecov.mq.t","mq_sendertime")
+sameissue_coefficients <- c("edgecov.same.issue.area.t","sameissuearea_sendertime")
+absdiffmq_coefficients <- c("nodeicov.AbsDiffMQscores","AbsDiffMQscores_sendertime")
+numberjusticespro_coefficients <- c("nodeicov.NumberJusticesPro","NumberJusticesPro_sendertime")
+
+# example trend CI plot for MQ coef
+mq_coef_trend <- matrix(NA,length(sender_times),3)
+colnames(mq_coef_trend) <- c("mean","lower","upper")
+for(t in sender_times){
+  effect.t <- bootstrapResults[,mq_coefficients[1]]+t*bootstrapResults[,mq_coefficients[2]]
+  mq_coef_trend[t,] <- c(mean(effect.t),quantile(effect.t,prob=c(0.025,.975)))
+}
+
+pdf("./images/mq_coef_trend_noEndo.pdf",width=6,height=3.5)
+par(las=1,mar=c(4,4,1,1))
+plot(sender_years,mq_coef_trend[,1],type="n",ylab="Effect",xlab="Year", main="Martin-Quinn Score" ,ylim=c(min(c(mq_coef_trend)),max(c(mq_coef_trend))))
+polygon(c(sender_years,rev(sender_years)),c(mq_coef_trend[,2],rev(mq_coef_trend[,3])),col="lightblue",border=NA)
+lines(sender_years,mq_coef_trend[,1],lwd=2,col="grey50")
+abline(h=0,lty=2,lwd=2)
+dev.off()
+
+
+# trend CI plot for edges coef
+edges_coef_trend <- matrix(NA,length(sender_times),3)
+colnames(edges_coef_trend) <- c("mean","lower","upper")
+for(t in sender_times){
+  effect.t <- bootstrapResults[,density_coefficients[1]]+t*bootstrapResults[,density_coefficients[2]]
+  edges_coef_trend[t,] <- c(mean(effect.t),quantile(effect.t,prob=c(0.025,.975)))
+}
+
+pdf("./images/edges_coef_trend_noEndo.pdf",width=6,height=3.5)
+par(las=1,mar=c(4,4,1,1))
+plot(sender_years,edges_coef_trend[,1],type="n",ylab="Effect",xlab="Year", main="Edges" ,ylim=c(min(c(edges_coef_trend)),max(c(edges_coef_trend))))
+polygon(c(sender_years,rev(sender_years)),c(edges_coef_trend[,2],rev(edges_coef_trend[,3])),col="lightblue",border=NA)
+lines(sender_years,edges_coef_trend[,1],lwd=2,col="grey50")
+abline(h=0,lty=2,lwd=2)
+dev.off()
+
+
+
+# trend CI plot for sameissuearea coef
+sameissue_coef_trend <- matrix(NA,length(sender_times),3)
+colnames(sameissue_coef_trend) <- c("mean","lower","upper")
+for(t in sender_times){
+  effect.t <- bootstrapResults[,sameissue_coefficients[1]]+t*bootstrapResults[,sameissue_coefficients[2]]
+  sameissue_coef_trend[t,] <- c(mean(effect.t),quantile(effect.t,prob=c(0.025,.975)))
+}
+
+pdf("./images/sameissue_coef_trend_noEndo.pdf",width=6,height=3.5)
+par(las=1,mar=c(4,4,1,1))
+plot(sender_years,sameissue_coef_trend[,1],type="n",ylab="Effect",xlab="Year", main="Same Issue Area" ,ylim=c(min(c(sameissue_coef_trend)),max(c(sameissue_coef_trend))))
+polygon(c(sender_years,rev(sender_years)),c(sameissue_coef_trend[,2],rev(sameissue_coef_trend[,3])),col="lightblue",border=NA)
+lines(sender_years,sameissue_coef_trend[,1],lwd=2,col="grey50")
+abline(h=0,lty=2,lwd=2)
+dev.off()
+
+
+
+# trend CI plot for absdiffmq coef
+absdiffmq_coef_trend <- matrix(NA,length(sender_times),3)
+colnames(absdiffmq_coef_trend) <- c("mean","lower","upper")
+for(t in sender_times){
+  effect.t <- bootstrapResults[,absdiffmq_coefficients[1]]+t*bootstrapResults[,absdiffmq_coefficients[2]]
+  absdiffmq_coef_trend[t,] <- c(mean(effect.t),quantile(effect.t,prob=c(0.025,.975)))
+}
+
+pdf("./images/absdiffmq_coef_trend_noEndo.pdf",width=6,height=3.5)
+par(las=1,mar=c(4,4,1,1))
+plot(sender_years,absdiffmq_coef_trend[,1],type="n",ylab="Effect",xlab="Year", main="Absolute Difference in Martin Quinn Scores" ,ylim=c(min(c(absdiffmq_coef_trend)),max(c(absdiffmq_coef_trend))))
+polygon(c(sender_years,rev(sender_years)),c(absdiffmq_coef_trend[,2],rev(absdiffmq_coef_trend[,3])),col="lightblue",border=NA)
+lines(sender_years,absdiffmq_coef_trend[,1],lwd=2,col="grey50")
+abline(h=0,lty=2,lwd=2)
+dev.off()
+
+
+# trend CI plot for absdiffmq coef
+numberjusticespro_coef_trend <- matrix(NA,length(sender_times),3)
+colnames(numberjusticespro_coef_trend) <- c("mean","lower","upper")
+for(t in sender_times){
+  effect.t <- bootstrapResults[,numberjusticespro_coefficients[1]]+t*bootstrapResults[,numberjusticespro_coefficients[2]]
+  numberjusticespro_coef_trend[t,] <- c(mean(effect.t),quantile(effect.t,prob=c(0.025,.975)))
+}
+
+pdf("./images/numberjusticespro_coef_trend_noEndo.pdf",width=6,height=3.5)
+par(las=1,mar=c(4,4,1,1))
+plot(sender_years,numberjusticespro_coef_trend[,1],type="n",ylab="Effect",xlab="Year", main="Number of Justices in Majority" ,ylim=c(min(c(numberjusticespro_coef_trend)),max(c(numberjusticespro_coef_trend))))
+polygon(c(sender_years,rev(sender_years)),c(numberjusticespro_coef_trend[,2],rev(numberjusticespro_coef_trend[,3])),col="lightblue",border=NA)
+lines(sender_years,numberjusticespro_coef_trend[,1],lwd=2,col="grey50")
+abline(h=0,lty=2,lwd=2)
+dev.off()
